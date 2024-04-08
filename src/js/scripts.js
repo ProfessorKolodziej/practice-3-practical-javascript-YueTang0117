0924565f214f05634ef7d9097c299fb1479ba1b8
YueTang0117 <ty0117@bu.edu>
2024-04-08 19:42:21 -0400

    Completed Practice 3

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,34 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+const myName = "Yue";
+let myAge = 23;
+
+console.log(myAge);
+
+//Practical Javascript
+
+/*Practical
+Javascript*/
+
+//show and hide an element on the page
+
+//Find the element to hide
+//document.querySelector searches for the first item that matches
+const toggledParagraph = document.querySelector(".show");
+console.log(toggledParagraph);
+
+//function that toggles the show/hide classes
+function toggleParagraph(){
+	toggledParagraph.classList.toggle("hide");
+	toggledParagraph.classList.toggle("show");
+	console.log("my toggle function");
+	console.log(toggledParagraph);
+}
+
+//Find the button and toggle the paragraph when we click it
+const toggleButton = document.querySelector("#toggle-control");
+
+toggleButton.addEventListener("click", toggleParagraph);
+
